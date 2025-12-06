607fc79e7e9d09577cbd115608223f711c2351d7
Ashmitha <136832594+ashmi-t@users.noreply.github.com>
2025-12-06 19:19:25 +0530

    Time: 58 ms (23.77%), Space: 18.4 MB (23.02%) - LeetHub

---
+++ 0467-unique-substrings-in-wraparound-string/0467-unique-substrings-in-wraparound-string.py
@@ -0,0 +1,10 @@
+class Solution:    
+    def findSubstringInWraproundString(self, p: str) -> int:
+        from collections import defaultdict
+        d = defaultdict(int)
+        streak = 0 
+        for i in range(len(p)):
+            streak = streak + 1 if (ord(p[i - 1]) - 96) % 26 == (ord(p[i]) - 97) else 1
+            d[p[i]] = max(d[p[i]], streak)
+        return sum(d.values())
+        
